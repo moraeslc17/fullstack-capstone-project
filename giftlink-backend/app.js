7c3b75f0361dfa41d2e6de4e601dffda95f1ab8b
Leticia Cristina Franca Moraes Dos Santos <lleehmoraes@gmail.com>
2025-07-10 20:44:46 -0400

    adding auth route

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -5,32 +5,34 @@ const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const { loadData } = require("./util/import-mongo/index");
+const {loadData} = require("./util/import-mongo/index");
+
 
 const app = express();
-app.use("*", cors());
+app.use("*",cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
-.catch((e) => console.error('Failed to connect to DB', e));
+    .catch((e) => console.error('Failed to connect to DB', e));
+
 
 app.use(express.json());
 
 // Route files
-const giftRoutes = require('./routes/giftRoutes'); // ✅ Gift API Task 1
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
+const giftRoutes = require('./routes/giftRoutes');
+const authRoutes = require('./routes/authRoutes');
 const searchRoutes = require('./routes/searchRoutes');
-
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
+
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-app.use('/api/gifts', giftRoutes); // ✅ Gift API Task 2
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
+app.use('/api/gifts', giftRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/search', searchRoutes);
 
 // Global Error Handler
@@ -39,9 +41,9 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-app.get("/", (req, res) => {
-    res.send("Inside the server");
-});
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
